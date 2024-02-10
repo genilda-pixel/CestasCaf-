27bf7bf91f792e59618279b6f18a4804fe905ded
Genilda <146936379+genilda-pixel@users.noreply.github.com>
2024-02-10 17:46:30 +0000

    CestasCafé atuaçização

---
+++ CestasCafé.Infrastructure/Data/ApplicationDbContest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CestasCafé.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+    }
+}
