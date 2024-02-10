d02556c5d6e86ffc33cc7e8a625ed06b6af673c6
Genilda <146936379+genilda-pixel@users.noreply.github.com>
2024-02-10 13:49:26 +0000

    CestasCafé 1

---
+++ CestasCafé.Domain/Entidades/Cestas.cs
@@ -0,0 +1,16 @@
+﻿namespace CestasCafé.Domain.Entidades
+{
+    public class Cestas
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public string? Descrição { get; set; }
+        public double Preço  { get; set; }  
+        public int Datas { get; set;}
+        public int Ocasião { get; set;}
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+            
+    }
+}
