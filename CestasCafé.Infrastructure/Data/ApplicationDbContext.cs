fca98d9d77344df06be5f710e32806e4e89f256c
Genilda <146936379+genilda-pixel@users.noreply.github.com>
2024-02-11 16:22:47 +0000

    CestasCafé /Domingo

---
+++ CestasCafé.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CestasCafé.Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace CestasCafé.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cestas> Cesta { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Cestas>().HasData();
+        }
+    }
+}
---
+++ CestasCafé.Infrastructure/Migrations/20240210194115_AddCestasToDb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CestasCafé.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCestasToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cesta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descrição = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Preço = table.Column<double>(type: "float", nullable: false),
+                    Datas = table.Column<int>(type: "int", nullable: false),
+                    Ocasião = table.Column<int>(type: "int", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cesta", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cesta");
+        }
+    }
+}
